ea2d3f384c1a26bdc3121ce0bfd973794f34c1e0
pontus erlandsson <wnedpro@live.se>
2019-02-28 12:17:45 +0100

    Created a simple chat using jgroups and a launcher to test it with. Group communication sort of works, still need to turn it into a replicaManager instead of a chat.

--- src/replicaManager/ReplicaManager.java
+++ src/replicaManager/ReplicaManager.java
@@ -1,5 +1,57 @@
 package replicaManager;
 
-public class ReplicaManager {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.jgroups.JChannel;
+import org.jgroups.Message;
+import org.jgroups.ReceiverAdapter;
+import org.jgroups.View;
+
+public class ReplicaManager extends ReceiverAdapter {
+	JChannel channel;
+	String user_name = System.getProperty("user.name", "n/a");
+	boolean eventLoopBool = true;
+
+	public ReplicaManager() {
+		try {
+			this.channel = new JChannel().setReceiver(this);
+			this.channel.connect("replicaManagerCluster");
+			eventLoop();
+			channel.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void eventLoop() {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		while (eventLoopBool) {
+			try {
+				System.out.println("> ");
+				String line = "";
+				line = in.readLine().toLowerCase();
+				if (line.startsWith("quit") || line.startsWith("exit")) {
+					eventLoopBool = false;
+					break;
+				}
+				line = "[" + user_name + "]" + line;
+				// null = broadcast line = string som vi ska konvertera
+				Message msg = new Message(null, line);
+				channel.send(msg);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+	public void viewAccepted(View new_view) {
+		System.out.println("** View: " + new_view);
+	}
+
+	public void receive(Message msg) {
+		System.out.println(msg.getSrc() + ": " + msg.getObject());
+	}
 
 }
