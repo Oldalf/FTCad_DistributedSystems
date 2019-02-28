129f67b4b37704010b14a2164aa72196b68dd195
Oldalf <wnedpro@live.se>
2019-02-28 16:18:33 +0100

    replica managers can identify if primary left view

--- src/replicaManager/ReplicaManager.java
+++ src/replicaManager/ReplicaManager.java
@@ -2,16 +2,29 @@ package replicaManager;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
+import org.jgroups.Address;
 import org.jgroups.JChannel;
 import org.jgroups.Message;
 import org.jgroups.ReceiverAdapter;
 import org.jgroups.View;
 
-public class ReplicaManager extends ReceiverAdapter {
-	JChannel channel;
-	String user_name = System.getProperty("user.name", "n/a");
-	boolean eventLoopBool = true;
+public class ReplicaManager extends ReceiverAdapter implements Runnable {
+	private JChannel channel;
+	private String user_name = System.getProperty("user.name", "n/a");
+	private boolean eventLoopBool = true;
+	private boolean threadLoopBool = true;
+	private boolean primaryMissing = true;
+	private Address primaryAddress;
+	private View previousView;
+	
+	/*
+	 * replica manager id stuff
+	 */
+	static final AtomicLong NEXT_ID = new AtomicLong(0);
+	private final long id = NEXT_ID.getAndIncrement();
 
 	public ReplicaManager() {
 		try {
@@ -47,11 +60,26 @@ public class ReplicaManager extends ReceiverAdapter {
 	}
 
 	public void viewAccepted(View new_view) {
+		if(previousView!= null && primaryAddress != null) {
+			List<Address> leavingMembers = View.leftMembers(previousView, new_view);
+			if(leavingMembers.contains(primaryAddress)) {
+				primaryMissing = true;
+			}
+		}
+		previousView = new_view;
 		System.out.println("** View: " + new_view);
 	}
 
 	public void receive(Message msg) {
 		System.out.println(msg.getSrc() + ": " + msg.getObject());
+	}
+
+	@Override
+	public void run() {
+		while (threadLoopBool) {
+			
+		}
+
 	}
 
 }
