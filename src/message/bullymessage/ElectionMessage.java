e4e8d969e2fc3530da0beaae37df16aaf8334bbd
Remes <joakimremes@hotmail.com>
2019-03-12 13:18:18 +0100

    v1.6

--- src/message/bullymessage/ElectionMessage.java
+++ src/message/bullymessage/ElectionMessage.java
@@ -6,22 +6,22 @@ import org.jgroups.Address;
 
 import message.MessagePayload;
 
-public class ElectionMessasge extends BullyMessage {
+public class ElectionMessage extends BullyMessage {
 	private static UUID messageUUID = UUID.fromString("11dd613e-3f65-11e9-b210-d663bd873d93"); 
 	private String m_id; 
 	private Address m_address;
 	
 	
 	//Used by jackson to create prototype objects.
-	protected ElectionMessasge() {
-		super(ElectionMessasge.messageUUID);
+	protected ElectionMessage() {
+		super(ElectionMessage.messageUUID);
 	}
-	protected ElectionMessasge(UUID uuid) {
+	protected ElectionMessage(UUID uuid) {
 		super(uuid);
 	}
 	//Use this constructor 
-	public ElectionMessasge(Address address, String id) {
-		super(ElectionMessasge.messageUUID);
+	public ElectionMessage(Address address, String id) {
+		super(ElectionMessage.messageUUID);
 	}
 	@Override
 	protected void executeForFrontend() {
