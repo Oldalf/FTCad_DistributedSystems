81301d1f7e5bc8af61eebeb8b40008c52c0a1f5f
Remes92 <joakimremes@hotmail.com>
2019-05-11 12:25:50 +0200

    RequestCleaner remove request.
    Added the remove request logic

--- src/Cleaner/RequestCleaner.java
+++ src/Cleaner/RequestCleaner.java
@@ -3,20 +3,37 @@ import java.util.TimerTask;
 
 import State.rmReplicableState;
 import replicaManager.RequestContainer;
+import replicaManager.RequestContainer.RequestStage;
 
 	
 
 public class RequestCleaner extends TimerTask {
 	private RequestContainer rc;
+	private int notConfirmedCounter = 0;
+	private boolean isAlive = true;
 	public RequestCleaner(RequestContainer requestContainer) {
 		this.rc = requestContainer;
 	}
 
 	@Override
 	public void run() {
-		rmReplicableState state = rmReplicableState.getInstance();
-		if(state.Object2Request_state.containsKey(rc.getObject())) {
-			state.Object2Request_state.remove(rc.getObject());
+		while(isAlive) {
+			if(rc.getStage() == RequestStage.ConfirmedToFrontEnd || notConfirmedCounter>5) {
+				rmReplicableState state = rmReplicableState.getInstance();
+				isAlive = false;
+				if(state.Object2Request_state.containsKey(rc.getObject())) {
+					state.Object2Request_state.remove(rc.getObject());
+					
+				}
+			}else {
+				try {
+					notConfirmedCounter++;
+					Thread.sleep(10000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 }
