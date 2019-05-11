c7a01d0b5a5cc518939dd5723b04ea72849dbcdb
Remes92 <joakimremes@hotmail.com>
2019-05-11 12:29:15 +0200

    Added global variables.
    Added global variables.

--- src/Cleaner/RequestCleaner.java
+++ src/Cleaner/RequestCleaner.java
@@ -8,6 +8,10 @@ import replicaManager.RequestContainer.RequestStage;
 	
 
 public class RequestCleaner extends TimerTask {
+	private final int NOT_CONFIRMED_LIMIT = 5;
+	private final int THREAD_SLEEP_TIME = 10000;
+	
+	
 	private RequestContainer rc;
 	private int notConfirmedCounter = 0;
 	private boolean isAlive = true;
@@ -18,17 +22,16 @@ public class RequestCleaner extends TimerTask {
 	@Override
 	public void run() {
 		while(isAlive) {
-			if(rc.getStage() == RequestStage.ConfirmedToFrontEnd || notConfirmedCounter>5) {
+			if(rc.getStage() == RequestStage.ConfirmedToFrontEnd || notConfirmedCounter>NOT_CONFIRMED_LIMIT) {
 				rmReplicableState state = rmReplicableState.getInstance();
 				isAlive = false;
 				if(state.Object2Request_state.containsKey(rc.getObject())) {
 					state.Object2Request_state.remove(rc.getObject());
-					
 				}
 			}else {
 				try {
 					notConfirmedCounter++;
-					Thread.sleep(10000);
+					Thread.sleep(THREAD_SLEEP_TIME);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
